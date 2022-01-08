f84ba7e107e9015bdd39ed09602c415ca9a7345e
gengzhenfei <phbgeng@gmail.com>
2022-01-08 18:08:26 +0800

    Z 字形变换，这题找规律，像是数学的数列递推公式，找到规律代码思路就很简单了

--- c++/L006ZigzagConversion.cpp
+++ c++/L006ZigzagConversion.cpp
@@ -11,9 +11,32 @@ public:
     L006ZigzagConversion(/* args */);
     ~L006ZigzagConversion();
 
+    /**
+     *
+      解题思路： 找规律
+      例如对一个4行的
+        0     6      12       18
+        1   5 7   11 13    17 19
+        2 4   8 10   14 16    20
+        3     9      15       21
+      对 k = i%(2n-2)进行判断：
+        k<=n-1时候，s[i]就属于第k行；
+        k>n-1时候，s[i]就属于2n-2-k行；
+     */
     string convert(string s, int numRows)
     {
-        return s;
+        int s_len = s.size();
+        if (s_len <= numRows || numRows == 1) return s;
+        vector<string> result;
+        for (int i = 0; i < numRows; i++) { result.push_back(""); }
+        for (int i = 0; i < s_len; i++) {
+            int k = i % (2 * numRows - 2);
+            if (k > numRows - 1) k = 2 * numRows - 2 - k;
+            result[k] += s[i];
+        }
+        string row_str;
+        for (string str : result) row_str += str;
+        return row_str;
     }
 };
 
@@ -21,17 +44,16 @@ L006ZigzagConversion::L006ZigzagConversion(/* args */) {}
 
 L006ZigzagConversion::~L006ZigzagConversion() {}
 
-// #include <iostream>
-// #include <string>
+// include <iostream>
 // #include "L006ZigzagConversion.cpp"
 // using namespace std;
 
 // int main()
 // {
-//     string str("baba");
-//     int numRows = 3;
-//     L006ZigzagConversion conversion;
-//     conversion.convert(str, numRows);
+//     string s("PAYPALISHIRING");
+//     int rows = 40;
+//     L006ZigzagConversion ZigzagConversion;
+//     cout << ZigzagConversion.convert(s, rows) << endl;
 
 //     return 0;
 // }
--- c++/main.cpp
+++ c++/main.cpp
@@ -1,30 +1,13 @@
 #include <iostream>
-#include "L018FourSum.cpp"
+#include "L006ZigzagConversion.cpp"
 using namespace std;
 
 int main()
 {
-    // vector<int> nums{1, 0, -1, 0, -2, 2};
-    // int target = 0;
-    // vector<int> nums{2, 2, 2, 2, 2};
-    // int target = 8;
-    // vector<int> nums{-3, -1, 0, 2, 4, 5};
-    // int target = 0;
-    // vector<int> nums{-2, -1, -1, 1, 1, 2, 2};
-    // int target = 0;
-    // vector<int> nums{-1, 0, 1, 2, -1, -4};
-    // int target = -1;
-    // vector<int> nums{1, -2, -5, -4, -3, 3, 3, 5};
-    // int target = -11;
-    // vector<int> nums{1000000000, 1000000000, 1000000000, 1000000000};
-    vector<int> nums{0, 0, 0, 1000000000, 1000000000, 1000000000, 1000000000};
-    int target = 1000000000;
-    L018FourSum FourSum;
-    vector<vector<int>> result = FourSum.fourSum(nums, target);
-    for (auto i : result) {
-        for (auto j : i) cout << j << " ";
-        cout << endl;
-    }
+    string s("PAYPALISHIRING");
+    int rows = 40;
+    L006ZigzagConversion ZigzagConversion;
+    cout << ZigzagConversion.convert(s, rows) << endl;
 
     return 0;
 }
