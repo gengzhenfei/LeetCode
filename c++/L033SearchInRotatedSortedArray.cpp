38fbffe46b149298629fd858ea1737e791e66595
gengzhenfei <phbgeng@gmail.com>
2022-01-15 14:12:54 +0800

    33.搜索旋转排序数组，借鉴二分查找思路 O(logn)

---
+++ c++/L033SearchInRotatedSortedArray.cpp
@@ -0,0 +1,62 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class L033SearchInRotatedSortedArray
+{
+private:
+    /* data */
+public:
+    L033SearchInRotatedSortedArray(/* args */);
+    ~L033SearchInRotatedSortedArray();
+
+    int search(vector<int> &nums, int target)
+    {
+        int nums_len = nums.size();
+        int start = 0, end = nums_len - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            // cout << "start:" << start << " end:" << end << " mid:" << mid << endl;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > nums[start]) { // nums[mid]左侧为升序
+                if (mid != end && (nums[start] > target || nums[mid] < target)) {
+                    // 注意 mid != end 否则mid+1>end 越界
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            } else { // nums[mid] 右侧为升序
+                if (mid != start && (nums[mid] > target || nums[end] < target)) {
+                    // 注意 mid != start 否则mid+1<start 越界
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+            // cout << "start:" << start << " end:" << end << " mid:" << mid << endl;
+        }
+        return -1;
+    }
+};
+
+L033SearchInRotatedSortedArray::L033SearchInRotatedSortedArray(/* args */) {}
+
+L033SearchInRotatedSortedArray::~L033SearchInRotatedSortedArray() {}
+
+// main.cpp
+// 调试时将以下代码放入main.cpp文件
+// #include <iostream>
+// #include "L033SearchInRotatedSortedArray.cpp"
+// using namespace std;
+
+// int main()
+// {
+//     vector<int> nums{3, 1};
+//     int target = 1;
+//     L033SearchInRotatedSortedArray SearchInRotatedSortedArray;
+//     int result = SearchInRotatedSortedArray.search(nums, target);
+//     cout << result << endl;
+//     return 0;
+// }
--- c++/main.cpp
+++ c++/main.cpp
@@ -1,17 +1,15 @@
 // main.cpp
 // 调试时将以下代码放入main.cpp文件
 #include <iostream>
-#include "L030SubstringWithConcatenationOfAllWords.cpp"
+#include "L033SearchInRotatedSortedArray.cpp"
 using namespace std;
 
 int main()
 {
-    // string str = "wordgoodgoodgoodbestword";
-    // vector<string> words{"word", "good", "best", "word"};
-    string str = "barfoothefoobarman";
-    vector<string> words{"foo", "bar"};
-    L030SubstringWithConcatenationOfAllWords SubstringWithConcatenationOfAllWords;
-    vector<int> result = SubstringWithConcatenationOfAllWords.findSubstring(str, words);
-    for (auto v : result) cout << v << endl;
+    vector<int> nums{3, 1};
+    int target = 1;
+    L033SearchInRotatedSortedArray SearchInRotatedSortedArray;
+    int result = SearchInRotatedSortedArray.search(nums, target);
+    cout << result << endl;
     return 0;
 }
