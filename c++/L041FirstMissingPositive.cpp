2a6803d09ccd766bb048a6e185dcdb5238922d73
gengzhenfei <phbgeng@gmail.com>
2022-01-21 00:16:46 +0800

    原地交换 构建哈希数组，再进行一趟遍历寻找最小缺失正整数

--- c++/L041FirstMissingPositive.cpp
+++ c++/L041FirstMissingPositive.cpp
@@ -13,25 +13,48 @@ public:
 
     /**
      * 最烂的方法：先排序，再遍历查找
+     * 不符合题意“时间复杂度O(n)”
+     */
+    // int firstMissingPositive(vector<int> &nums)
+    // {
+    //     sort(nums.begin(), nums.end());
+    //     for (int i = 0; i < nums.size(); i++) {
+    //         if (nums[i] > 0) {
+    //             if (nums[i] != 1) { // 第一个正数不等于1
+    //                 return 1;
+    //             } else {
+    //                 int offset = i - nums[i];
+    //                 for (; i < nums.size() && i - nums[i] == offset; i++) {
+    //                     // 跳过重复的数字
+    //                     for (; i < nums.size() - 1 && nums[i] == nums[i + 1]; i++) { offset++; }
+    //                 }
+    //                 return i - offset;
+    //             }
+    //         }
+    //     }
+    //     return 1;
+    // }
+
+    /**
+     * 原地交换 构建本地哈希数组
      */
     int firstMissingPositive(vector<int> &nums)
     {
-        sort(nums.begin(), nums.end());
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] > 0) {
-                if (nums[i] != 1) { // 第一个正数不等于1
-                    return 1;
-                } else {
-                    int offset = i - nums[i];
-                    for (; i < nums.size() && i - nums[i] == offset; i++) {
-                        // 跳过重复的数字
-                        for (; i < nums.size() - 1 && nums[i] == nums[i + 1]; i++) { offset++; }
-                    }
-                    return i - offset;
-                }
-            }
+        int len = nums.size();
+        for (int i = 0; i < len; i++) {
+            while (nums[i] > 0 && nums[i] <= len && nums[nums[i] - 1] != nums[i]) { swap(nums[i], nums[nums[i] - 1]); }
         }
-        return 1;
+        // for (auto i : nums) cout << i << endl;
+        int index = 0;
+        for (; index < len && nums[index] == index + 1; index++) {}
+        return index + 1;
+    }
+
+    void swap(int &a, int &b)
+    {
+        int tmp = a;
+        a = b;
+        b = tmp;
     }
 };
 
