d4391289ff14d3fc8f4c8ed3193046fad6166b9c
gengzhenfei <phbgeng@gmail.com>
2022-02-25 18:11:04 +0800

    91.解码方法，添加参考答案注释

--- c++/L091DecodeWays.cpp
+++ c++/L091DecodeWays.cpp
@@ -30,6 +30,11 @@ public:
         for (int k = 1; k <= 2; k++) { dfs(s, res, i + n, k); }
     }
 
+    /**
+     * https://leetcode-cn.com/problems/decode-ways/comments/15627
+     * 用DP去做，可以发现就是约束版的d(n) = d(n-1) + d(n-2);
+     * 其中 如果是s[n-1]为0，d(n-1) = 0，d(n) = d(n-2)，因为0无法单独解码，而d(n-2)的条件则是必须在1与26之间，否则d(n) = d(n-1)
+     */
     void dp(string &s, int &res)
     {
         if (s.size() == 0 || (s.size() == 1 && s[0] == '0')) return;
@@ -40,9 +45,9 @@ public:
         vector<int> d(s.size() + 1, 0);
         d[0] = 1;
         for (int i = 1; i < d.size(); i++) {
-            d[i] = s[i - 1] == '0' ? 0 : d[i - 1];
+            d[i] = s[i - 1] == '0' ? 0 : d[i - 1]; // s[n-1]为0，d(n-1) = 0，d(n) = d(n-2)
             if (i > 1 && (s[i - 2] == '1' || (s[i - 2] == '2' && s[i - 1] <= '6'))) {
-                //
+                // d(n) = d(n-1) + d(n-2)
                 d[i] += d[i - 2];
             }
         }
